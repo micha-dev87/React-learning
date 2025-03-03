f859f70d80b4db0ac8c1b6a58da48b7cec9c0c16
angel <angeltamgho87@gmail.com>
2025-03-03 16:33:20 -0500

    Ajout du workflow GitHub Actions pour déploiement automatique

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/React-learning/', // Attention à la casse qui doit correspondre à votre repo
   server: {
     host: true,
     port: 3000,
